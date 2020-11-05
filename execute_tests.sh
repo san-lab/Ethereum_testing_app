131449750b4aa3c872499934c9b73baf46375b4f
Ubuntu <ethuser@ETH2.agekj0ic44ievme5ynwa1dtt1g.ax.internal.cloudapp.net>
2020-11-05 15:37:18 +0000

    Fixing the tmux script

--- execute_tests.sh
+++ execute_tests.sh
@@ -7,7 +7,7 @@ session="work"
 tmux start-server
 
 # create a new tmux session, starting vim from a saved session in the new window
-tmux new-session -d -s $session -n vim #"vim -S ~/.vim/sessions/kittybusiness"
+tmux new-session -d -s $session
 
 # Select pane 1, set dir to api, run vim
 tmux selectp -t 1
@@ -20,16 +20,22 @@ tmux selectp -t 2
 # Split pane 2 vertically by 50%
 tmux splitw -v -p 50
 
-# select pane 3, set to api root
-tmux selectp -t 3
-tmux send-keys "api" C-m 
+# select pane 0, set to api root
+tmux selectp -t 0
 
-# Select pane 1
-tmux selectp -t 1
-# Split pane 1 vertically by 50%
+# Split pane 3 vertically by 50%
 tmux splitw -v -p 50
 
+tmux selectp -t 0
+tmux send-keys "python3 akv_ethereum_signing.py 100 deploy santander https://rinkeby.infura.io/v3/f2a8581c640340758bead17199084148" C-m 
+
 tmux selectp -t 1
+tmux send-keys "python3 akv_ethereum_signing.py 100 deploy santander https://rinkeby.infura.io/v3/f2a8581c640340758bead17199084148" C-m 
+
+tmux selectp -t 2
+tmux send-keys "python3 akv_ethereum_signing.py 100 deploy santander https://rinkeby.infura.io/v3/f2a8581c640340758bead17199084148" C-m 
+
+tmux selectp -t 3
 tmux send-keys "python3 akv_ethereum_signing.py 100 deploy santander https://rinkeby.infura.io/v3/f2a8581c640340758bead17199084148" C-m 
 
 # Finished setup, attach to the tmux session!
