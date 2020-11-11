252af05389fbaa1d909e8401ff3ec913bd427c62
pegartillo95 <pegarc03@ucm.es>
2020-11-11 10:09:06 +0100

    Adding sleep

--- change_key_runtime.sh
+++ change_key_runtime.sh
@@ -14,10 +14,10 @@ tmux selectp -t 1
 tmux splitw -h -p 50
 
 tmux selectp -t 0
-tmux send-keys "python3 akv_ethereum_signing.py 10000 deploy akv test http://52.157.68.69:8545" C-m 
+tmux send-keys "python3 akv_ethereum_signing.py 2000 deploy akv test http://52.157.68.69:8545" C-m 
 
 tmux selectp -t 1
-tmux send-keys "python3 akv_ethereum_signing.py create hsm-key" C-m 
+tmux send-keys "sleep 20 && python3 akv_ethereum_signing.py create hsm-key" C-m 
 
 # Finished setup, attach to the tmux session!
 tmux attach-session -t $session
