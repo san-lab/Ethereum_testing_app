cd720b238ef3dfbf0e6b00ea99e04e867dad880c
pegartillo95 <pegarc03@ucm.es>
2020-11-11 11:49:08 +0100

    Changing the key we modify

--- change_key_runtime.sh
+++ change_key_runtime.sh
@@ -14,10 +14,10 @@ tmux selectp -t 1
 tmux splitw -h -p 50
 
 tmux selectp -t 0
-tmux send-keys "python3 akv_ethereum_signing.py deploy 2000 akv test http://52.157.68.69:8545" C-m 
+tmux send-keys "python3 akv_ethereum_signing.py deploy 2000 akv bankia http://52.157.68.69:8545" C-m 
 
 tmux selectp -t 1
-tmux send-keys "sleep 20 && python3 akv_ethereum_signing.py create hsm-key" C-m 
+tmux send-keys "sleep 20 && python3 akv_ethereum_signing.py create bankia" C-m 
 
 # Finished setup, attach to the tmux session!
 tmux attach-session -t $session
