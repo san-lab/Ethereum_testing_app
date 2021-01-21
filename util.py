db245f171ea20e304fe9cb6f5b3b715075dacd76
pegartillo95 <pegarc03@ucm.es>
2021-01-21 11:49:52 +0100

    fixing convertion to ethereum standard

--- util.py
+++ util.py
@@ -48,10 +48,9 @@ def convert_azure_secp256k1_signature_to_vrs(pub_key_bytes, msg_hash_bytes, sig_
     print("1111")
     if len(sig_bytes)<64:
         return 0,0,0, False
-        print("XXXXX")
     sig_ser = ecdsa_pubkey.ecdsa_deserialize_compact(sig_bytes)
     print("1111")
-    verified_ecdsa = ecdsa_pubkey.ecdsa_verify(msg_hash_bytes, sig_ser, raw=True)
+    #verified_ecdsa = ecdsa_pubkey.ecdsa_verify(msg_hash_bytes, sig_ser, raw=True)
     print("2222")
     v = -1
     unrelated = MyECDSA()
